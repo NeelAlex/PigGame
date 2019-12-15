765ce62385dd23bee7da60663b19bb718026580b
Neel Alex <neelalex007@gmail.com>
2019-12-15 12:18:41 +0530

    update readme

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ GAME RULES
 3. note that if a player rolls 1, then his 'round score' will be lost. and the other player gets the next chance.
 4. the player can also chose to 'hold' in which case, his 'round score' gets added to his 'global score' and its the other players turn.
 5. the first player to obtain 100 'global score' wins.
+
+Note:
+You need to score exactly 100 points to win. for eg: If your global score is 98,  you need to get exactly 2 in the next roll of dice to win.
+If you obtain 3 or more in your next roll, you cant win. As your global score goes above 100 in such a case. 
 */
 
 var scores, roundScore, activePlayer;
